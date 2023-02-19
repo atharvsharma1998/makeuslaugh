9cebb03c408ece822963e8a09a5f485864a02b7b
Atharv Sharma <31643037+atharvsharma1998@users.noreply.github.com>
2023-02-19 10:22:43 +0000

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+public static void main(String args[]) {
+	  
+	  System.out.println("Interviewer asked me to write code for printing a star * pattern");
+	  
+	  System.out.println("I came up this soltuion");
+	  System.out.println("System.out.println(\"*\\n**\\n***\\n****\\n\");");
+	  
+	  System.out.println("Haven't heard from them in a while");
+  }
+}
